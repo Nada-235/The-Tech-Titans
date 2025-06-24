a899e6341b6df8773904af35120eaee8c006d8ba
mustafa Kenany <mustafa.kenany2022@gmail.com>
2025-06-24 15:39:37 +0300

    solve 4 diffrent questions

--- _42StringsQuestions/Program.cs
+++ _42StringsQuestions/Program.cs
@@ -5,7 +5,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        CompareTwoStringUsingLinguistic_Casesensitive_Ordinal();
+        CenteryOfYear();
     }
 
     /// <summary>Compares the two sting by culture.</summary>
@@ -64,7 +64,7 @@ class Program
         Console.WriteLine("Notes 0 means Equal, 1 means 1st string greater, -1 means 2nd string is greater"); //notes when print
         int LinguisticResult = string.Compare(word_03, word_04, true, _Us); //case-insensitive
         Console.WriteLine($"LinguisticResult {getResult(LinguisticResult)}");
-        int CasesensitiveResult = string.Compare(word_03, word_04,false,_Us); //case-sensitive
+        int CasesensitiveResult = string.Compare(word_03, word_04, false, _Us); //case-sensitive
         Console.WriteLine($"CasesensitiveResult {getResult(CasesensitiveResult)}");
         int OrdinalResult = string.CompareOrdinal(word_03, word_04); //Ordinal
         Console.WriteLine($"OrdinalResult {getResult(OrdinalResult)}");
@@ -79,14 +79,79 @@ class Program
                     break;
                 case 1:
                     return 1;
-                   break;
+                    break;
                 default:
                     return -1;
-                   break;
+                    break;
             }
         }
 
     }
 
+    /// <summary>Checks the chars.</summary>
+    static void CheckChars()
+    {
+        Console.Write("Enter string to check chars if unique or not: ");
+        string charsArray = Console.ReadLine(); //enter  string
+        int flag = 0; //Flag when is identical 
+
+        for (int i = 0; i < charsArray.Length; i++) // trace chars in string
+        {
+            for (int j = i + 1; j < charsArray.Length; j++)
+            {
+                if (charsArray[j] == charsArray[i])
+                {
+                    flag = 1;
+                }
+            }
+        }
+        if (flag == 0)
+        {
+            Console.WriteLine("There is no Duplicate chars in Entered String");
+        }
+        else
+        {
+            Console.WriteLine("There is a Duplicate chars in Entered String");
+        }
+    }
+    static void CharsToAscII()
+    {
+        Console.Write("Enter string to convert to Ascii: ");
+        string charsArray = Console.ReadLine(); //enter  string
+        for (int i = 0; i < charsArray.Length; i++)
+        {
+            int decimalVal = charsArray[i];
+            Console.Write(string.Format("{0:X}", decimalVal) + " ");
+        }
+    }
+    static void PrintRepetationOfChars()
+    {
+        Console.Write("Enter string to Check Chars Repetions: ");
+        string charsArray = Console.ReadLine(); //enter  string
+        charsArray = charsArray.Replace(" ", ""); //remove whitespaces 
+        string repetedChars=string.Empty;
+        for (int i = 0; i < charsArray.Length; i++)
+        {
+            for (int j = i + 1; j < charsArray.Length; j++)
+            {
+                if (charsArray[j] == charsArray[i])
+                {
+                    Console.WriteLine(charsArray[j]);
+                }
+                
+            }
+        }
+        
+    }
+
+    static void CenteryOfYear()
+    {
+        Console.Write("Enter Year: ");
+        int _year =int.Parse(Console.ReadLine()); // read year from user
+
+        int century = (_year + 99) / 100; // converting
+
+        Console.WriteLine($"Century is {century}");
+    }
 }
 
